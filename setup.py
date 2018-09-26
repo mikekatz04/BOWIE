207d19cf338443f3089d43ae83503c0f6c0c8893
Michael Katz <mikekatz04@gmail.com>
2018-09-25 19:29:59 -0500

    restructuring complete

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup, Extension
 
-extmodule1 = Extension('pyphenomd.phenomd',
-	libraries = ['gsl'],
-	library_dirs = ['/usr/lib', '/usr/local/lib'],
-	sources=['pyphenomd/phenomd/phenomd.c'])
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -16,9 +12,9 @@ setup(name='bowie',
 	author='Michael Katz',
 	author_email='mikekatz04@gmail.com',
 	url='https://github.com/mikekatz04/BOWIE',
-	packages=['bowie_makeplot', 'bowie_makeplot.plotutils','pyphenomd', 'pyphenomd.noise_curves','bowie_gencondata', 'bowie_gencondata.genconutils'],
-	ext_modules=[extmodule1],
-	py_modules=['bowie_makeplot.make_plot','bowie_gencondata.generate_contour_data', 'pyphenomd.pyphenomd', 'pyphenomd.read_noise_curves'],
+	packages=['bowie_makeplot', 'bowie_makeplot.plotutils', 'bowie_gencondata', 'bowie_gencondata.genconutils'],
+	py_modules=['bowie_makeplot.make_plot','bowie_gencondata.generate_contour_data'],
+	install_requires=['pyphenomd',],
 	include_package_data=True,
 	classifiers=[
         "Programming Language :: Python :: 3",
