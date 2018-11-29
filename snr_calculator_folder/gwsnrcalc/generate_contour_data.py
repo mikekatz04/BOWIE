02498933dbfbf5bf1621bbb3605a80074f31e1ff
Michael Katz <mikekatz04@gmail.com>
2018-11-29 13:21:30 -0600

    generate_contour_data updated, pep8ed, documented

--- snr_calculator_folder/gwsnrcalc/generate_contour_data.py
+++ snr_calculator_folder/gwsnrcalc/generate_contour_data.py
@@ -1,77 +1,72 @@
 """
-Generate gridded data for contour plots with PhenomD waveform. It is part of the BOWIE analysis tool. Author: Michael Katz. Please cite "Evaluating Black Hole Detectability with LISA" (arXiv:1807.02511) for usage of this code.
+Generate gridded data for contour plots with PhenomD waveform.
+It is part of the BOWIE analysis tool.
+Author: Michael Katz. Please cite "Evaluating Black Hole Detectability with LISA" (arXiv:1807.02511)
+for usage of this code.
 
-PhenomD waveforms are generated according to Husa et al 2016 (arXiv:1508.07250) and Khan et al 2016 (arXiv:1508.07253). Please cite these papers if the PhenomD waveform is used.
+PhenomD waveforms are generated according to Husa et al 2016 (arXiv:1508.07250) and Khan et al 2016
+(arXiv:1508.07253). Please cite these papers if the PhenomD waveform is used.
 
-generate_contour_data produces gridded data sets based on an input dict from a script or .json configuration file. It can take any basic set of parameters for binary black holes and produce waveforms and SNR calculations for each phase of binary black hole coalescence. It reads in sensitivity curves from .txt files. The outputs can either be .txt or .hdf5. It can run in parallel or on a single processor. See generate_contour_data_guide.ipynb for examples on how to use this code. See generate_data_for_paper.ipynb for the generation of the data used in the paper.
+generate_contour_data produces gridded data sets based on an input class or dictionary.
+It can take any basic set of parameters for binary black holes and produce waveforms
+and SNR calculations for each phase of binary black hole coalescence.
+It reads in sensitivity curves from .txt files. The outputs can either be .txt or .hdf5.
+It can run in parallel or on a single processor. See the example notebooks for usage of this module.
 
 This code is licensed under the GNU public license.
 
 """
 
 import sys
-from collections import OrderedDict
-import h5py
 import json
-from multiprocessing import Pool
-import numpy as np
-import datetime
 import time
-from scipy.interpolate import interp1d
-import scipy.constants as ct
-#import pdb
 
-from astropy.cosmology import Planck15 as cosmo
-from astropy.io import ascii
+from gwsnrcalc.genconutils.genprocess import GenProcess
+from gwsnrcalc.genconutils.readout import FileReadOut
+from gwsnrcalc.genconutils.forminput import MainContainer as GenInput
 
-import os
-
-from snr_calculator.genconutils.genprocess import GenProcess
-from snr_calculator.genconutils.readout import FileReadOut
-
-Msun=1.989e30
 
 def generate_contour_data(pid):
     """
     Main function for this program.
 
-    Input:
-        :param pid: (dict) - contains all of the input information for the generation from a dict in a script or .json configuration file. See BOWIE documentation for all possibilities with the pid.
+    This will read in sensitivity_curves and binary parameters; calculate snrs
+    with a matched filtering approach; and then read the contour data out to a file.
+
+    Args:
+        pid (obj or dict): GenInput class or dictionary containing all of the input information for
+            the generation. See BOWIE documentation and example notebooks usage of
+            this class.
+
     """
+    # check if pid is  dicionary or GenInput class
+    # if GenInput, change to dictionary
+    if isinstance(pid, GenInput):
+        pid = pid.return_overall_dictionary()
 
     begin_time = time.time()
-    global WORKING_DIRECTORY
-
-    gid = pid['generate_info']
-
-    if "output_folder" not in pid['output_info']:
-        pid['output_info']['output_folder'] = "."
-
-    if "input_folder" not in pid['input_info']:
-        pid['input_info']['input_folder'] = "."
-
-    if "output_file_type" not in pid['output_info']:
-        pid['output_info']['output_file_type'] = "hdf5"
 
     WORKING_DIRECTORY = '.'
     if 'WORKING_DIRECTORY' not in pid['general'].keys():
         pid['general']['WORKING_DIRECTORY'] = WORKING_DIRECTORY
 
-    #instantiate
+    # Generate the contour data.
     running_process = GenProcess(pid)
     running_process.set_parameters()
     running_process.run_snr()
 
-    #read out
-    file_out = FileReadOut(pid, pid['output_info']['output_file_type'], WORKING_DIRECTORY + '/' + pid['output_info']['output_file_name'],  running_process.xvals, running_process.yvals, running_process.final_dict, running_process.num_x, running_process.num_y)
+    # Read out
+    file_out = FileReadOut(running_process.xvals, running_process.yvals,
+                           running_process.final_dict,
+                           **{**pid['general'], **pid['generate_info'], **pid['output_info']})
 
-    #adding extras to output info
-    file_out.prep_output()
-
-    print('outputing file:', WORKING_DIRECTORY + '/' + pid['output_info']['output_file_name'])
-    getattr(file_out, pid['output_info']['output_file_type'] + '_read_out')()
+    print('outputing file:', pid['general']['WORKING_DIRECTORY'] + '/'
+          + pid['output_info']['output_file_name'])
+    getattr(file_out, file_out.output_file_type + '_read_out')()
 
     print(time.time()-begin_time)
+    return
+
 
 if __name__ == '__main__':
     """
