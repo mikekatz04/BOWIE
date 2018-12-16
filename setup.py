154a51e82919cd16658a484abb59078126712657
Michael Katz <mikekatz04@gmail.com>
2018-12-16 21:54:53 +0100

    setup updated. pip install first check

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 from setuptools import setup, Extension
 
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(name='bowie',
-	version='1.2.7',
-	description='Binary Observability with Illustrative Exploration (BOWIE)',
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	author='Michael Katz',
-	author_email='mikekatz04@gmail.com',
-	url='https://github.com/mikekatz04/BOWIE',
-	packages=['bowie_makeplot', 'bowie_makeplot.plotutils', 'bowie_gencondata', 'bowie_gencondata.genconutils'],
-	py_modules=['bowie_makeplot.make_plot','bowie_gencondata.generate_contour_data'],
-	install_requires=['pyphenomd','numpy', 'scipy', 'collections', 'sys', 'json', 'multiprocessing', 'datetime', 'time', 'astropy', 'h5py', 'matplotlib','ctypes'],
-	include_package_data=True,
-	classifiers=[
+      version='2.0.0',
+      description='Binary Observability with Illustrative Exploration (BOWIE)',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Michael Katz',
+      author_email='mikekatz04@gmail.com',
+      url='https://github.com/mikekatz04/BOWIE',
+      packages=['bowie_makeplot', 'bowie_makeplot.plotutils'],
+      py_modules=['bowie_makeplot.make_plot'],
+      install_requires=['gwsnrcalc', 'numpy', 'scipy', 'astropy', 'h5py', 'matplotlib'],
+      include_package_data=True,
+      classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
-    ]
-	)
-
+        ]
+      )
